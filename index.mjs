3496ae2d91d4028e2396a3b25051967cbc7df7b9
Adrian Estrada <edsadr@gmail.com>
2024-10-22 20:14:19 -0500

    01 -  Hello world

--- index.mjs
+++ index.mjs
@@ -6,36 +6,55 @@ import 'dotenv/config'
 // Import printing tools
 import { marked } from 'marked'
 import { markedTerminal } from 'marked-terminal'
-
-marked.use(markedTerminal());
+import { bgCyan, bgYellow, white } from 'colorette'
 
 // Import AI providers SDKs
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import OpenAI from 'openai'
+marked.use(markedTerminal())
 
 // Prompt to generate content
-const prompt = 'Háblame sobre la comunidad MedellinJS';
+const prompt = 'Háblame sobre la comunidad MedellinJS'
 
 // Function to print replies
-function printReply(reply) {
+function printReply (reply) {
   console.log(marked.parse(reply))
 }
 
 // Generate content using Gemini
-async function helloGemini() {
-  // @TODO: Implement Gemini SDK usage
+async function helloGemini () {
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMNINI_API_KEY)
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+
+    const result = await model.generateContent(prompt)
+    console.log(bgYellow(white('Reply from Gemini:\n')))
+    printReply(result.response.text())
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+  }
 }
 
 // Generate content using OpenAI
-async function helloOpenAI() {
-  // @TODO: Implement OpenAI SDK usage
+async function helloOpenAI () {
+  try {
+    const openai = new OpenAI()
+    const result = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [{ role: 'user', content: prompt }]
+    })
+
+    console.log(bgCyan(white('Reply from OpenAI:\n')))
+    printReply(result.choices[0].message.content)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+  }
 }
 
 if (process.env.GEMNINI_API_KEY) {
-  helloGemini();
+  helloGemini()
 }
 
 if (process.env.OPENAI_API_KEY) {
-  helloOpenAI();
+  helloOpenAI()
 }
-
